2466e4461f675de32e7dc6937139c47aa77e4fd8
Shaxriyor Do'ngboyev <250261@newuu.uz>
2025-10-02 12:47:34 +0500

    .

--- problem12.cpp
+++ problem12.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
 using namespace std;
-int main() {
+int cubeOfDigits(int number) {
+    string s = to_string(number);
+    int l=s.length();
+    int t=10;
+    int j=1;
+    int sum=0;
+    for (int i = 0; i < l; i++) {
+            sum=sum+(number%t/j)*(number%t/j)*(number%t/j);
+            t*=10;
+            j*=10;
+        }
+    return sum;
+    }
 
+void isArmstrong(int sum,int number) {
+
+    if (sum==number) {
+        cout<<"It is Armstrong integer";
+    }
+    else {
+        cout<<"It is not Armstrong integer";
+    }
+}
+int main() {
+    int n;
+    cin>>n;
+    isArmstrong(cubeOfDigits(n),n);
+    return 0;
 }
